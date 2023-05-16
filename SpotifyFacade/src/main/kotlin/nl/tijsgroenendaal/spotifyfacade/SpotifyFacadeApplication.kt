a96f3716a87322869bec7709bc59d43907351fd4
tijsgroen <tijs.groenendaal@gmail.com>
2023-05-16 22:11:44 +0000

    Create SpotifyFacade module

---
+++ SpotifyFacade/src/main/kotlin/nl/tijsgroenendaal/spotifyfacade/SpotifyFacadeApplication.kt
@@ -0,0 +1,11 @@
+package nl.tijsgroenendaal.spotifyfacade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyFacadeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpotifyFacadeApplication>(*args)
+}
