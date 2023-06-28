416b436c9d55c9e467a4cdeff3ee2569ec3865de
tijsgroen <tijs.groenendaal@gmail.com>
2023-06-28 13:53:17 +0000

    improve session-song timeout config

--- QueueMusicFacade/src/main/kotlin/nl/tijsgroenendaal/queuemusicfacade/services/SessionSongService.kt
+++ QueueMusicFacade/src/main/kotlin/nl/tijsgroenendaal/queuemusicfacade/services/SessionSongService.kt
@@ -7,6 +7,7 @@ import nl.tijsgroenendaal.queuemusicfacade.repositories.SessionSongRepository
 import nl.tijsgroenendaal.qumu.exceptions.BadRequestException
 import nl.tijsgroenendaal.qumu.exceptions.SessionErrorCodes
 import nl.tijsgroenendaal.qumu.exceptions.SessionSongErrorCode
+import org.springframework.beans.factory.annotation.Value
 
 import org.springframework.stereotype.Service
 
@@ -14,15 +15,15 @@ import java.time.Duration
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-private val MIN_DURATION_TILL_NEXT_SONG = Duration.ofSeconds(900) // 15 minutes
-
 @Service
 class SessionSongService(
-    private val sessionSongRepository: SessionSongRepository
+    private val sessionSongRepository: SessionSongRepository,
+    @Value("\${queuemusic.session.song.timeout}")
+    private val songTimeout: Duration
 ) {
 
     private fun canDeviceCreateSong(deviceLink: UserDeviceLinkModel) {
-        val lowerBoundDateTime = LocalDateTime.now(ZoneOffset.UTC).minus(MIN_DURATION_TILL_NEXT_SONG)
+        val lowerBoundDateTime = LocalDateTime.now(ZoneOffset.UTC).minus(songTimeout)
         val sessionSongCount = sessionSongRepository.countByDeviceLinkIdAndCreatedAtAfter(deviceLink.id, lowerBoundDateTime)
 
         if (sessionSongCount >= 1)
