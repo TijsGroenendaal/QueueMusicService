2b8354d45dd14f93891204c5d3d43e5753e79eab
tijsgroen <tijs.groenendaal@gmail.com>
2023-08-02 20:38:18 +0000

    Rename jwt to token

--- IdpService/src/main/kotlin/nl/tijsgroenendaal/idpservice/query/responses/LoginQueryResponse.kt
+++ IdpService/src/main/kotlin/nl/tijsgroenendaal/idpservice/query/responses/LoginQueryResponse.kt
@@ -3,7 +3,7 @@ package nl.tijsgroenendaal.idpservice.query.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class LoginQueryResponse(
-    val jwt: String,
+    val token: String,
     @JsonProperty("refresh_token")
     val refreshToken: String,
     @JsonProperty("expires_in")
